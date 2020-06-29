a0620993910ca657297e4121052ddd9c92aec355
joker <2572702337@qq.com>
2020-06-29 11:59:56 +0800

    2020/6/29

--- untitled/src/Test.java
+++ untitled/src/Test.java
@@ -2,4 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+
+    public void getname(){
+        System.out.println("zs");
+    }
 }
